0f599f78ee042f9928e627cc72e7c543db5171b9
koyo-miyamura <nexus9071@gmail.com>
2020-02-22 17:30:29 +0900

    デフォルトポート番号の設定など

--- main.go
+++ main.go
@@ -10,20 +10,32 @@ import (
 	"github.com/koyo-miyamura/image_server/handler"
 )
 
+const defaultPort = 12345
+
 func main() {
-	if len(os.Args) != 2 {
-		log.Println("Please set port. ex) go run main.go 3000")
-		return
-	}
-	port, err := strconv.Atoi(os.Args[1])
+	port, err := getPort()
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
 	log.Printf("Server started! Port:%d", port)
 
 	server := handler.NewServer()
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
+}
+
+func getPort() (int, error) {
+	var (
+		port = defaultPort
+	)
+	if len(os.Args) == 2 {
+		port, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Println(err)
+			return 0, nil
+		}
+		return port, nil
+	}
+	return port, nil
 }
